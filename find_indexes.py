9b146dc5ab485e3163b86d616207909d66892c43
pushpendu91 <31428986+pushpendu91@users.noreply.github.com>
2018-06-10 18:34:03 +0530

    Add files via upload

---
+++ find_indexes.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python2
+
+
+import requests
+import json
+from requests.auth import HTTPBasicAuth
+import ast
+import re
+
+resp = requests.get('URL', auth=HTTPBasicAuth('userid', 'password'))
+
+if resp.status_code != 200:
+        print('GET /indexStatus/ {}'.format(resp.status_code))
+
+json_ob=resp.json()
+
+id_dict = {}
+id_list = []
+for index in json_ob["indexes"]:
+        id_list.append(index["id"])
+        id = index["id"]
+        data = str(index["definition"])
+
+        x = re.search('({.+})', data).group(0)
+        y = re.search(r'num_replica', x)
+        if y:
+            z = ast.literal_eval(str(x))
+            dict1 = {str(id) : z['num_replica']}
+            id_dict.update(dict1)
+
+
+for key,value in id_dict.items():
+    id_count = id_list.count(int(key))
+    if id_count == value + 1 :
+        print('OK')
+    else:
+        print('ALERT')
+
